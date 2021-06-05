6528b0867d146ca46237b9ce93628e4835354d0e
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-05 22:09:32 +0100

    Add useUser hook
    Based on https://github.com/vercel/next.js/tree/canary/examples/with-magic

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,32 @@
+import useSWR from 'swr';
+import type {MagicUserMetadata} from "@magic-sdk/types";
+
+/**
+ * Get the logged in user.
+ */
+export function useUser(): UseUserResponse {
+    const { data, error } = useSWR('/api/auth/user', fetcher);
+    const user: MagicUserMetadata|undefined = data?.user;
+
+    return {
+        user,
+        error,
+        finished: Boolean(data),
+        hasUser: Boolean(user),
+    };
+}
+
+export interface UseUserResponse {
+    user: MagicUserMetadata|undefined;
+    error: any,
+    finished: boolean,
+    hasUser: boolean,
+}
+
+const fetcher = (url) => fetch(
+    url
+).then(
+    res => res.json()
+).then(
+    data => ({ user: data?.user || null })
+);
