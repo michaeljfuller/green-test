3851ecc21a247b9ce993e593c57966efa95aebcd
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-05 15:08:04 +0100

    Add SASS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import css from '../styles/Home.module.scss'
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <div className={css.root}>
             <Head>
                 <title>Green Test</title>
                 <meta name="description" content="Test for Green"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <main className={styles.main}>
+            <main>
 
-                <Image src="/green.svg" alt="Green Logo" width={135} height={34}/>
+                <Image src="/green.svg" alt="Green Logo" width={135} height={34} />
 
             </main>
         </div>
