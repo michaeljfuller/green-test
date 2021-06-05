d58b96045b850d7708669ee7ef81aba622c45614
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-05 22:48:24 +0100

    Add SignInContext.Provider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import SignInContext, {defaultSignIn} from "../src/contexts/SignInContext";
 
 function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return <>
+        <SignInContext.Provider value={defaultSignIn}>
+            <Component {...pageProps} />
+        </SignInContext.Provider>
+    </>;
 }
 
 export default MyApp
