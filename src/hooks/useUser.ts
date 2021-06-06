828556ed091c4b972ae57ab0af2025d846e33aca
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-06 01:32:04 +0100

    Make useUser only dispatch setAuth once
    Wasn't a problem because useUser was only used once at the top level, but could've caused it to accidentally set to authenticating if used on something else that mounted.
    Moved into fetcher.

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -2,33 +2,29 @@ import useSWR from 'swr';
 import type {MagicUserMetadata} from "@magic-sdk/types";
 import useStoreDispatch from "./useStoreDispatch";
 import {setAuthError, setAuthenticating, setUser} from "../store/user";
-import {useEffect} from "react";
 
-/**
- * Get the logged in user.
- */
+/** Get the logged in user. */
 export function useUser(): UseUserResponse {
     const dispatch = useStoreDispatch();
 
-    useEffect(() => {
+    const fetcher = (url) => {
         dispatch(setAuthenticating(true));
-    }, [])
-
-    const fetcher = (url) => fetch(
-        url
-    ).then(
-        res => res.json()
-    ).then(
-        data => ({ user: data?.user || null })
-    ).then(
-        data => {
-            dispatch(setUser(data.user));
-            return data;
-        },
-    ).catch(error => {
-        dispatch(setAuthError(error));
-        throw error;
-    });
+        return fetch(
+            url
+        ).then(
+            res => res.json()
+        ).then(
+            data => ({ user: data?.user || null })
+        ).then(
+            data => {
+                dispatch(setUser(data.user));
+                return data;
+            },
+        ).catch(error => {
+            dispatch(setAuthError(error));
+            throw error;
+        });
+    };
 
     const { data, error } = useSWR('/api/auth/user', fetcher);
     const user: MagicUserMetadata|undefined = data?.user;
